436eaee7131def349e5027a56d8157126b62625f
emmanguyen <emma.mphuong@gmail.com>
2016-10-30 21:27:14 -0700

    Remove an old version

--- demo-app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField
-
-
-class TravelForm(Form):
-    username = StringField('Name of travler')
-    city = StringField('City where you are ? (San Francisco/ LA/ New York/ Boston/ Chicago)')
-    
